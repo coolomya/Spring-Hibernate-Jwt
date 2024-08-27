66223607d1bd33475bfdc21b20f81db95b308d95
Omey Bhosale <47236054+coolomya@users.noreply.github.com>
2024-08-27 21:20:58 +0530

    Update UserRepository.java change1

--- src/main/java/com/example/Repo/UserRepository.java
+++ src/main/java/com/example/Repo/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.Models.UserEntity;
@@ -9,6 +10,6 @@ import com.example.Models.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 	
-	@Query(value = "SELECT * FROM users WHERE username = ?1", nativeQuery = true)
-    UserEntity findByUsername(String username);
+	@Query(value = "select u.*, array_agg(r.role_name) as roles from users u inner join users_roles ur on u.username = :username and ur.users_id = u.id inner join roles r on r.id = ur.roles_id group by 1,2,3,4", nativeQuery = true)
+    UserEntity findByUsername(@Param("username") String username);
 }
