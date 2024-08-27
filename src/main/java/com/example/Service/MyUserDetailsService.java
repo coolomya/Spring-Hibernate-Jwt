6c45449e02cdf59beab3db8cfaf23973abf6da2e
Omey Bhosale <47236054+coolomya@users.noreply.github.com>
2024-08-27 21:20:17 +0530

    Update MyUserDetailsService.java change1

--- src/main/java/com/example/Service/MyUserDetailsService.java
+++ src/main/java/com/example/Service/MyUserDetailsService.java
@@ -1,5 +1,11 @@
 package com.example.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,16 +17,22 @@ import com.example.Repo.UserRepository;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+	@Autowired
 	private UserRepository userRepository;
 	 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username){
     	System.out.println("Inside service loadUserByUsername " + username);
     	UserEntity user = userRepository.findByUsername(username);
+    	System.out.println("User fetched: " + user);
         if (user == null) {
-            throw new UsernameNotFoundException("User not found with username: " + username);
+            //throw new UsernameNotFoundException("User not found with username: " + username);
+        	System.out.println("User not found");
+        	return null;
         }
-        return (UserDetails) user;
-        
+        //user.get
+        List<SimpleGrantedAuthority> authorities = user.getRoles().stream().map(r -> new SimpleGrantedAuthority(r.getRoleName())).collect(Collectors.toList());
+        UserDetails ud = new User(user.getUsername(), user.getPassword(), authorities);
+        return ud;
     }
 }
